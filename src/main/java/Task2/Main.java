d6212a180175422435f594bd956ffdd10dc11960
Sergei Shagov <ssshagov@gmail.com>
2022-08-28 17:56:36 +0300

    Added Task2

---
+++ src/main/java/Task2/Main.java
@@ -0,0 +1,24 @@
+package Task2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {5, 6, 3, 2, 5, 1, 4, 9};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int tmp = 0;
+                if (arr[i] > arr[j]) {
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+}
